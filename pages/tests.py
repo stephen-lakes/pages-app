a7301e49e3c926a890cc5d0cc3c4c86fc812eb2c
stephen-lakes <oluyomiolamilekan99@gmail.com>
2022-03-06 00:48:00 +0100

    added tests for home page and about page

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
+from django.test import SimpleTestCase
+# SimpleTestCase is used because we are not using a database. 
 # Create your tests here.
+
+class SimpleTests(SimpleTestCase):
+    def test_home_page_status_code(self):
+        repsonse = self.client.get('/')
+
+        # check whether home page exist
+        self.assertEqual(repsonse.status_code, 200)
+
+    def test_about_page_status_code(self):
+
+        # check whether about page exist
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code, 200)
+
