708afd4091300b4859fc2d5e7eec3d7133859d15
stephen-lakes <oluyomiolamilekan99@gmail.com>
2022-03-06 00:34:34 +0100

    initial commit

---
+++ pages/views.py
@@ -0,0 +1,17 @@
+from pipes import Template
+from re import template
+from django.shortcuts import render
+
+from django.views.generic import TemplateView
+# Create your views here.
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
+
+
+class ContactPageView(TemplateView):
+    template_name = 'contact.html'
